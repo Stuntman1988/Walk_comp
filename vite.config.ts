060a181a89f2fb2ea8663c8da2ea6647f862b1d7
Robin lundell <robin.lundell@hotmail.com>
2025-02-08 12:27:56 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Walk_comp/', // 👈 Sätt detta direkt för att säkerställa rätt sökväg vid deploy
+  base: '/Walk_comp',
   server: {
     port: 5174,
   },
